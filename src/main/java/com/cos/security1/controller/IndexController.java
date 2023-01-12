99859ddde5d904805e019c5fce99221118ac50f9
hyunWoo <nhw0926@gmail.com>
2023-01-13 02:23:22 +0900

    [일감] #2 스프링부트 시큐리티 2강 - 시큐리티 설정 .antMatchers("/user/**").authenticated() // 로그인한경우? .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')") // 권한 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") // 권한 .anyRequest().permitAll()  // 위 3개의 주소가아니면 어디든 접근가능하다. .and().formLogin().loginPage("/login"); // 로그인페이지 설정

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.cos.security1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -13,4 +14,33 @@ public class IndexController {
         return "index"; // src/main/resources/templates/index.mustache
     }
 
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨!!";
+    }
 }
