8041037020438091cbfb8627cdedf093be35fda1
hyunWoo <nhw0926@gmail.com>
2023-01-16 23:31:13 +0900

    [일감] #5 스프링부트 시큐리티 7강 - 구글 회원 프로필 정보 받아보기

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.security1.config;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity  // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)  // secured 어노테이션 활성화 , (preAuthorize, postAuthorize) 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     @Bean // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
     public BCryptPasswordEncoder encodePwd() {
@@ -33,6 +38,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/") // 로그인 성공 시 / 로 이동하지만 (localhost:8080/user 호출하여 로그인페이지 이동 후 로그인하면 localhost:8080/user 로 내가 호출햇던 페이지로 이동함)
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm");   // 구글 로그인이 완룓된 뒤의 처리가 필요함.
-    }
+                .loginPage("/loginForm")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);   // 구글 로그인이 완룓된 뒤의 처리가 필요함. Tip. 코드를 받지않고 (엑세스토근+사용자프로필정보 를 한번에 받는다)
+                // 1.코드받기(인증)[이 사람이 로그인이 되었다. 구글에 로그인이 된 정상적인 사용자다 ], 2.엑세스토근받기(권한)[사용자 정보에 접근가능한 권한], 3. 2에서 받은 권한을 통해 사용자 프로필 정보를 가져와
+                // 4-1.그 정보를 토대로 회원가입을 자동으로 진행시키기도 함. 4-2. (이메일, 전화번호, 이름, 아이디) 만 가져왓을때 내가 쇼핑몰을하면 -> 집주소가필요, 백화점몰이면 -> 등급이필요요                //
+    }           // 위 내용처럼 추가적인 정보가 필요하다면, 따로 회원가입을 진행해야한다.
 }
---
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -0,0 +1,34 @@
+package com.cos.security1.config.oauth;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
+
+    // 구글로 부터 받은 userRequest 데이터에 대한 후처리되는 함수
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        System.out.println("userRequest:"+userRequest); // userRequest:org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest@4a0dcaf8
+        System.out.println("getClientRegistration:"+userRequest.getClientRegistration());   // ClientRegistration{registrationId='google', clientId='31849756497-ebjd3nuesqimtd0f4v7go4lahkvl771j.apps.googleusercontent.com', clientSecret='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', clientAuthenticationMethod=org.springframework.security.oauth2.core.ClientAuthenticationMethod@4fcef9d3, authorizationGrantType=org.springframework.security.oauth2.core.AuthorizationGrantType@5da5e9f3, redirectUri='{baseUrl}/{action}/oauth2/code/{registrationId}', scopes=[email, profile], providerDetails=org.springframework.security.oauth2.client.registration.ClientRegistration$ProviderDetails@3ea219cf, clientName='Google'}
+        System.out.println("getAccessToken:"+userRequest.getAccessToken()); // org.springframework.security.oauth2.core.OAuth2AccessToken@9834117b
+        System.out.println("getTokenValue:"+userRequest.getAccessToken().getTokenValue());  // ya29.a0AX9GBdXJ8yey7IN1IcyqTprE5YMwlkc8yWe_hWi7L26istO6pXYZTcdXpmo-reMnRibP22LVvOwZTDEjV5wMWU79MLYXCpNQAp89W78DZ6nWKs_seobz6EfhzvKVDKR5-zMNGZrNspItOl-w4SJRLfGyZrRgaCgYKAcsSARMSFQHUCsbC5abOcfxjpIGFLybXqR_a9A0163
+        System.out.println("getAttributes:"+super.loadUser(userRequest).getAttributes()); // {sub=109208456048493571184, name=나현우, given_name=현우, family_name=나, picture=https://lh3.googleusercontent.com/a/AEdFTp66iUD785qhwUyttkrNkKrdtmBpm5j4FatBoSP8=s96-c, email=nhw0926@gmail.com, email_verified=true, locale=ko}
+
+        /**
+         * username = "google_109208456048493571184"
+         * password = 암호화(겟인데어)
+         * email = "nhw0926@gmail.com"
+         * role = "ROLE_USER"
+         * provider = "google"
+         * providerId = "109208456048493571184"
+         */
+        
+        // 회원가입을 강제로 진행해볼 예정
+
+        return super.loadUser(userRequest);
+    }
+}
